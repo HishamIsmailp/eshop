ea3864e3ba411f0576ee36bbd62744b6721a6add
Hisham <hisham@kiebot.com>
2022-11-16 16:23:23 +0530

    Hisham | product view and product detail view added

--- src/main/java/in/upcode/eshop/controllers/ProdctController.java
+++ src/main/java/in/upcode/eshop/controllers/ProdctController.java
@@ -3,20 +3,36 @@ package in.upcode.eshop.controllers;
 import in.upcode.eshop.models.Product;
 import in.upcode.eshop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+//@RestController
+@Controller
 public class ProdctController {
     @Autowired
     ProductService productService;
 
-    @GetMapping("products")
+//    @GetMapping("products")
+//    public List<Product> GetAllProducts(){
+//       return productService.GetAllProducts();
+//    }
+    @RequestMapping("products")
+    @ResponseBody
     public List<Product> GetAllProducts(){
-       return productService.GetAllProducts();
+    return productService.GetAllProducts();
+}
+
+    @RequestMapping("view/products")
+    public String displayProducts(Model model){
+        List<Product> products=productService.GetAllProducts();
+        model.addAttribute("products",products);
+        return "products";
     }
+
 
     @PostMapping("products")
     public void PostProduct(@RequestBody Product product){
@@ -32,6 +48,13 @@ public class ProdctController {
     @GetMapping("products/{id}")
     public Optional<Product> GetAllProducts(@PathVariable Long id){
         return productService.GetProduct(id);
+    }
+
+    @RequestMapping("view/products/{id}")
+    public String displayProductDetail(@PathVariable Long id,Model model){
+        Optional<Product> product=productService.GetProduct(id);
+        model.addAttribute("product",product.get());
+        return "productdetail";
     }
 
     @DeleteMapping("products/{id}")
